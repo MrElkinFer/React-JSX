6b8b2734eaa06ddd2a17b5335aeaae654879c9b9
Elkin Arismendy <elk.orion@gmail.com>
2024-02-20 15:43:06 -0500

    Agregando componente saludar

--- mi-primera-app/src/App.js
+++ mi-primera-app/src/App.js
@@ -1,22 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import HolaMundo,{Adios} from './components/HolaMundo'; //solo puedo tener una función por defecto, si voy a llamar una que no sea la función por defecto tengo que llamarla entre orejas
+import Saludar from './components/Saludar';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Saludar/> 
       </header>
     </div>
   );
