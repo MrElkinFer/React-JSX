881a80b91c4f01e8d63d8ed424a3292e0f11a4c8
Elkin Arismendy <elk.orion@gmail.com>
2024-02-20 19:17:29 -0500

    Agregando componente botón

--- mi-primera-app/src/App.js
+++ mi-primera-app/src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 import HolaMundo,{Adios} from './components/HolaMundo'; //solo puedo tener una función por defecto, si voy a llamar una que no sea la función por defecto tengo que llamarla entre orejas
 import Saludar from './components/Saludar';
 function App() {
+  const user = {
+    nombre: "Fernando Arismendy",
+    edad: 36,
+    color: "Azul"
+
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Saludar/> 
+        <Saludar UserInfo= {user} /> 
       </header>
     </div>
   );
